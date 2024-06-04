cffb529705bb6a7e320bebe2615e9bd6b238269a
Victor Marotta <victordossantosmarotta@gmail.com>
2024-06-04 13:51:40 -0300

    feat: Adiciona localização por GPS do usuario

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -219,6 +219,7 @@ export default function Map() {
                   position.coords.latitude,
                 ]);
                 setLocalization(newCenter);
+                setView((current) => ({ ...current, center: newCenter }));
               });
             }}
           >
